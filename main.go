cba7eac3ec5753aa84f07494d4c087fae02f6b54
flameprincess <unpredictableanonymous639@gmail.com>
2025-01-31 14:39:51 +0500

    impove main.go

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"log"
 	"main/cmd/server"
 	"main/pkg/db"
 	"main/pkg/utils/seeds"
 )
 
 func main() {
-	db.InitDb()
-	seeds.SeedLanguageData()
+	initialize()
 	server.InitServer()
+}
+
+func initialize() {
+	log.Println("Initializing database...")
+	if err := db.InitDb(); err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+
+	log.Println("Seeding initial data...")
+	if err := seeds.SeedLanguageData(); err != nil {
+		log.Fatalf("Failed to seed language data: %v", err)
+	}
 }
