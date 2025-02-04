160cefd11b2988703e3ccb26e16ff9b5a83a8392
flameprincess <unpredictableanonymous639@gmail.com>
2025-02-04 09:56:09 +0500

    add indexes

--- internal/customer/customer_model.go
+++ internal/customer/customer_model.go
@@ -14,7 +14,7 @@ type Customer struct {
 	StoreID    int16             `json:"store_id" gorm:"type:int2;not null"`
 	FirstName  string            `json:"first_name" gorm:"type:varchar(45);not null"`
 	LastName   string            `json:"last_name" gorm:"type:varchar(45);not null"`
-	Email      string            `json:"email" gorm:"type:varchar(50);not null;uniqueIndex"`
+	Email      string            `json:"email" gorm:"type:varchar(50);not null;unique;uniqueIndex"`
 	AddressID  int16             `json:"address_id" gorm:"type:int2;not null;foreignKey:AddressID"`
 	Activebool bool              `json:"activebool" gorm:"type:boolean;not null;default:true"`
 	CreateDate time.Time         `json:"create_date" gorm:"type:date;not null;default:current_date"`
