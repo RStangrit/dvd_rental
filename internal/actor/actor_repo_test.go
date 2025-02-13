18846d090378acb56dfaf6ff33d1aa41c4d6bc91
flameprincess <unpredictableanonymous639@gmail.com>
2025-02-13 16:51:06 +0500

    add Test_ReadAllActors unit test

--- internal/actor/actor_repo_test.go
+++ internal/actor/actor_repo_test.go
@@ -1,31 +1,15 @@
 package actor
 
 import (
+	"main/pkg/db"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 func Test_CreateActor(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("Error creating mock database: %v", err)
-	}
-	defer db.Close()
-
-	gormDB, err := gorm.Open(postgres.New(postgres.Config{
-		Conn: db,
-	}), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
-	if err != nil {
-		t.Fatalf("Error connecting to GORM: %v", err)
-	}
-
 	newActor := &Actor{
 		FirstName: "John",
 		LastName:  "Doe",
@@ -38,11 +22,39 @@ func Test_CreateActor(t *testing.T) {
 		WillReturnRows(sqlmock.NewRows([]string{"actor_id"}).AddRow(1))
 	mock.ExpectCommit()
 
-	err = CreateActor(gormDB, newActor)
+	err := CreateActor(gormDB, newActor)
 
 	assert.NoError(t, err)
 
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Fatalf("Something went wrong: %v", err)
+	}
+}
+
+func Test_ReadAllActors(t *testing.T) {
+	expectedActors := []Actor{
+		{ActorID: 1, FirstName: "John", LastName: "Doe"},
+		{ActorID: 2, FirstName: "Jane", LastName: "Smith"},
+	}
+
+	mock.ExpectQuery(`SELECT count\(\*\) FROM "actor"`).
+		WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(2))
+
+	mock.ExpectQuery(`SELECT \* FROM "actor" WHERE "actor"."deleted_at" IS NULL ORDER BY actor_id asc LIMIT \$1`).
+		WithArgs(10). // Pagination: limit=10
+		WillReturnRows(sqlmock.NewRows([]string{"actor_id", "first_name", "last_name"}).
+			AddRow(expectedActors[0].ActorID, expectedActors[0].FirstName, expectedActors[0].LastName).
+			AddRow(expectedActors[1].ActorID, expectedActors[1].FirstName, expectedActors[1].LastName))
+
+	pagination := db.Pagination{Page: 1, Limit: 10}
+
+	actors, total, err := ReadAllActors(gormDB, pagination)
+
+	assert.NoError(t, err)
+	assert.Equal(t, int64(2), total)
+	assert.Equal(t, expectedActors, actors)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %v", err)
 	}
 }
