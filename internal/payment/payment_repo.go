a5cc286d9b059b6cb069bb1f657e574305523dcf
flameprincess <unpredictableanonymous639@gmail.com>
2025-02-14 11:24:14 +0500

    payment refactoring

--- internal/payment/payment_repo.go
+++ internal/payment/payment_repo.go
@@ -2,31 +2,33 @@ package payment
 
 import (
 	"main/pkg/db"
+
+	"gorm.io/gorm"
 )
 
-func CreatePayment(newPayment *Payment) error {
-	return db.GORM.Table("payment").Create(&newPayment).Error
+func CreatePayment(db *gorm.DB, newPayment *Payment) error {
+	return db.Table("payment").Create(&newPayment).Error
 }
 
-func ReadAllPayments(pagination db.Pagination) ([]Payment, int64, error) {
+func ReadAllPayments(db *gorm.DB, pagination db.Pagination) ([]Payment, int64, error) {
 	var payments []Payment
 	var totalRecords int64
 
-	db.GORM.Table("payment").Count(&totalRecords)
-	err := db.GORM.Table("payment").Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order("payment_id asc").Find(&payments).Error
+	db.Table("payment").Count(&totalRecords)
+	err := db.Table("payment").Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order("payment_id asc").Find(&payments).Error
 	return payments, totalRecords, err
 }
 
-func ReadOnePayment(paymentID int64) (*Payment, error) {
+func ReadOnePayment(db *gorm.DB, paymentID int64) (*Payment, error) {
 	var payment Payment
-	err := db.GORM.Table("payment").First(&payment, paymentID).Error
+	err := db.Table("payment").First(&payment, paymentID).Error
 	return &payment, err
 }
 
-func UpdateOnePayment(payment Payment) error {
-	return db.GORM.Table("payment").Where("payment_id = ?", payment.PaymentID).Updates(payment).Error
+func UpdateOnePayment(db *gorm.DB, payment Payment) error {
+	return db.Table("payment").Where("payment_id = ?", payment.PaymentID).Updates(payment).Error
 }
 
-func DeleteOnePayment(payment Payment) error {
-	return db.GORM.Table("payment").Where("payment_id = ?", payment.PaymentID).Delete(&payment).Error
+func DeleteOnePayment(db *gorm.DB, payment Payment) error {
+	return db.Table("payment").Where("payment_id = ?", payment.PaymentID).Delete(&payment).Error
 }
