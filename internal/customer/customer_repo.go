2a013548baf47c50f17478441073018c93d2eee1
flameprincess <unpredictableanonymous639@gmail.com>
2025-02-14 10:50:13 +0500

    customer refactoring

--- internal/customer/customer_repo.go
+++ internal/customer/customer_repo.go
@@ -2,31 +2,33 @@ package customer
 
 import (
 	"main/pkg/db"
+
+	"gorm.io/gorm"
 )
 
-func CreateCustomer(newCustomer *Customer) error {
-	return db.GORM.Table("customer").Create(&newCustomer).Error
+func CreateCustomer(db *gorm.DB, newCustomer *Customer) error {
+	return db.Table("customer").Create(&newCustomer).Error
 }
 
-func ReadAllCustomers(pagination db.Pagination) ([]Customer, int64, error) {
+func ReadAllCustomers(db *gorm.DB, pagination db.Pagination) ([]Customer, int64, error) {
 	var customers []Customer
 	var totalRecords int64
 
-	db.GORM.Table("customer").Count(&totalRecords)
-	err := db.GORM.Table("customer").Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order("customer_id asc").Find(&customers).Error
+	db.Table("customer").Count(&totalRecords)
+	err := db.Table("customer").Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order("customer_id asc").Find(&customers).Error
 	return customers, totalRecords, err
 }
 
-func ReadOneCustomer(customerId int64) (*Customer, error) {
+func ReadOneCustomer(db *gorm.DB, customerId int64) (*Customer, error) {
 	var customer Customer
-	err := db.GORM.Table("customer").First(&customer, customerId).Error
+	err := db.Table("customer").First(&customer, customerId).Error
 	return &customer, err
 }
 
-func UpdateOneCustomer(customer Customer) error {
-	return db.GORM.Table("customer").Omit("customer_id").Updates(customer).Error
+func UpdateOneCustomer(db *gorm.DB, customer Customer) error {
+	return db.Table("customer").Omit("customer_id").Updates(customer).Error
 }
 
-func DeleteOneCustomer(customer Customer) error {
-	return db.GORM.Delete(&customer).Error
+func DeleteOneCustomer(db *gorm.DB, customer Customer) error {
+	return db.Delete(&customer).Error
 }
