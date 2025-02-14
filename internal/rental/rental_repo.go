fc7dccc8b82980f628b83a5a04ee0cb148ae5fb2
flameprincess <unpredictableanonymous639@gmail.com>
2025-02-14 11:25:48 +0500

    rental refactoring

--- internal/rental/rental_repo.go
+++ internal/rental/rental_repo.go
@@ -2,31 +2,33 @@ package rental
 
 import (
 	"main/pkg/db"
+
+	"gorm.io/gorm"
 )
 
-func CreateRental(newRental *Rental) error {
-	return db.GORM.Table("rental").Create(&newRental).Error
+func CreateRental(db *gorm.DB, newRental *Rental) error {
+	return db.Table("rental").Create(&newRental).Error
 }
 
-func ReadAllRentals(pagination db.Pagination) ([]Rental, int64, error) {
+func ReadAllRentals(db *gorm.DB, pagination db.Pagination) ([]Rental, int64, error) {
 	var rentals []Rental
 	var totalRecords int64
 
-	db.GORM.Table("rental").Count(&totalRecords)
-	err := db.GORM.Table("rental").Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order("rental_id asc").Find(&rentals).Error
+	db.Table("rental").Count(&totalRecords)
+	err := db.Table("rental").Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order("rental_id asc").Find(&rentals).Error
 	return rentals, totalRecords, err
 }
 
-func ReadOneRental(rentalID int64) (*Rental, error) {
+func ReadOneRental(db *gorm.DB, rentalID int64) (*Rental, error) {
 	var rental Rental
-	err := db.GORM.Table("rental").First(&rental, rentalID).Error
+	err := db.Table("rental").First(&rental, rentalID).Error
 	return &rental, err
 }
 
-func UpdateOneRental(rental Rental) error {
-	return db.GORM.Table("rental").Where("rental_id = ?", rental.RentalID).Updates(rental).Error
+func UpdateOneRental(db *gorm.DB, rental Rental) error {
+	return db.Table("rental").Where("rental_id = ?", rental.RentalID).Updates(rental).Error
 }
 
-func DeleteOneRental(rental Rental) error {
-	return db.GORM.Table("rental").Where("rental_id = ?", rental.RentalID).Delete(&rental).Error
+func DeleteOneRental(db *gorm.DB, rental Rental) error {
+	return db.Table("rental").Where("rental_id = ?", rental.RentalID).Delete(&rental).Error
 }
